9cfa7e0a87755c67f4648be584d78f0b12018797
HectorStiven <vigoyahecto90@gmail.com>
2024-03-16 17:13:22 -0500

    jeisonns dadadad dassd

--- biblioteca_compensar/settings.py
+++ biblioteca_compensar/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'estudiantes',  # Agrega la aplicación 'estudiantes' aquí
+    'corsheaders',  # Agrega esta línea
 ]
 
 MIDDLEWARE = [
@@ -139,5 +140,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 CORS_ALLOWED_ORIGIN = ['*']
