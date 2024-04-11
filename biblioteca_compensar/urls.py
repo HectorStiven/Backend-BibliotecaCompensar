b149a77230d58c2034aed4c0b6da15dbf1736f16
HectorStiven <vigoyahecto90@gmail.com>
2024-04-11 18:14:49 -0500

    modelos corergidos 100%

--- biblioteca_compensar/urls.py
+++ biblioteca_compensar/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-      path('estudiantes/', include('estudiantes.estudiantes_url.estudiantes_url')),
+      path('biblioteca/', include('estudiantes.estudiantes_url.estudiantes_url')),
 ]
