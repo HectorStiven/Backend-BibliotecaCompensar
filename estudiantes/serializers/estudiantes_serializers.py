323da582cc680b0c728ebb763a7735bab644e24d
HectorStiven <vigoyahecto90@gmail.com>
2024-04-25 01:21:09 -0500

    agragar valor a tabla boolean

--- estudiantes/serializers/estudiantes_serializers.py
+++ estudiantes/serializers/estudiantes_serializers.py
@@ -55,11 +55,15 @@ class CategoriaLibrosSerializer(serializers.ModelSerializer):
     class Meta:
         model = CategoriaLibros
         fields = '__all__'
-
 class LibroSerializer(serializers.ModelSerializer):
+    categoriaLibro = serializers.SlugRelatedField(slug_field='nombre_categoria', queryset=CategoriaLibros.objects.all())
+    Editorial = serializers.SlugRelatedField(slug_field='nombre', queryset=Editorial.objects.all())
+    id_Autor = serializers.PrimaryKeyRelatedField(queryset=Autor.objects.all())
+    id_estante = serializers.SlugRelatedField(slug_field='id_estante', queryset=Estantes.objects.all())
+
     class Meta:
         model = Libro
-        fields = '__all__'
+        fields = ['idISBN', 'titulo', 'disponibleEnBiblioteca', 'agno_publicacion', 'descripcion', 'estado_libro', 'cantidad_copias', 'categoriaLibro', 'Editorial', 'id_Autor', 'id_estante']
 
 class AutorSerializer(serializers.ModelSerializer):
     class Meta:
