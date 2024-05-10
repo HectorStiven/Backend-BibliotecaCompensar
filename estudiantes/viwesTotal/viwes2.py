1b5baa29f8e1abb10c65f0da076b546052cb1943
HectorStiven <vigoyahecto90@gmail.com>
2024-05-10 03:25:53 -0500

    cambios a servicios

--- estudiantes/viwesTotal/viwes2.py
+++ estudiantes/viwesTotal/viwes2.py
@@ -126,7 +126,14 @@ class ListarGradosVista(generics.ListAPIView):
     queryset = Grado.objects.all()
     serializer_class = GradoSerializer
 
-
+    def get(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'success': True,
+            'detail': 'Lista de Grados registradss',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
 class BorrarGradosVista(generics.DestroyAPIView):
     queryset = Grado.objects.all()
     serializer_class = GradoSerializer
