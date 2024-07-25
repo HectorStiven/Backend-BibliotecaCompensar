8e600a2bae35057ceea1ab72fce8e918eaa4f5fa
HectorStiven <vigoyahecto90@gmail.com>
2024-07-25 09:46:31 -0500

    final

--- estudiantes/apps.py
+++ estudiantes/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class EstudiantesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'estudiantes'
+    
+    def ready(self):
+        from estudiantes.jobs import scheduler
+        scheduler.start()
